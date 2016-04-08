a0882068cf87589f590725c45c51fcd7aacd7135
Matt Pindell <pindell.m@gmail.com>
2016-04-07 20:06:22 -0600

    test: 'They see the names of all artists'

--- spec/features/user_views_all_artists_spec.rb
+++ spec/features/user_views_all_artists_spec.rb
@@ -1,22 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "User views all artists" do
-  scenario "They see the page for all artists" do
-    artist_name       = "Bob Marley"
-    artist_image_path = "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
+  scenario "They see the names of all artists" do
+    artists = %w(Bob Adele Beyonce).map do |artist_name|
+      Artist.create(name: artist_name, image_path: "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg")
+    end
 
     visit artists_path
-    click_on "New artist"
-    fill_in "artist_name", with: artist_name
-    fill_in "artist_image_path", with: artist_image_path
-    click_on "Create Artist"
 
     expect(page).to have_content "All Artists"
-
-    click_on "All Artists"
-    
-    expect(page).to have_content "All Artists"
-    expect(page).to have_content "Bob Marley"
+    expect(page).to have_content "Bob"
+    expect(page).to have_content "Adele"
+    expect(page).to have_content "Beyonce"
   end
 
 
