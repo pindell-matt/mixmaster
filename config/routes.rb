964fe3cb6e31e840f6892dac16710c2ed0cfa16c
Matt Pindell <pindell.m@gmail.com>
2016-04-07 22:46:28 -0600

    adding songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :artists, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :artists do
+    resources :songs, only: [:new]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
