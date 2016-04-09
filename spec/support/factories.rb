6c09d834997f2bb331c836e115c21ca927be2bf5
Matt Pindell <pindell.m@gmail.com>
2016-04-09 15:54:41 -0600

    feature test: user creates a playlist

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,6 +1,20 @@
 FactoryGirl.define do
   factory :artist do
-    name       "Bob Marley"
+    name
     image_path "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
   end
+
+  sequence :name do |n|
+    "#{n} Artist"
+  end
+
+  sequence :title, ["A", "B", "C"].cycle do |n|
+    "#{n} Title"
+  end
+
+  factory :song do
+    title
+    artist
+  end
+
 end
