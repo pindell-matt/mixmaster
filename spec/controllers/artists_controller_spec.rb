77b788a662cf2be25b649969623f676afb5db58e
Matt Pindell <pindell.m@gmail.com>
2016-04-10 13:22:58 -0600

    created controller tests for 'GET' requests

---
+++ spec/controllers/artists_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe ArtistsController, type: :controller do
+  describe "GET #index" do
+    it "assigns all artists as @artists and renders the index template" do
+      artist = create(:artist)
+
+      get(:index)
+
+      expect(assigns(:artists)).to eq([artist])
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested artist as @artist and renders the show template" do
+      artist = create(:artist)
+      get(:show, {id: artist.to_param})
+      expect(assigns(:artist)).to eq(artist)
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns the new artist as @artist" do
+      get :new
+      expect(assigns(:artist)).to be_a_new(Artist)
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested artist as @artist" do
+      artist = create(:artist)
+      get :edit, {:id => artist.to_param}
+      expect(assigns(:artist)).to eq(artist)
+    end
+  end
+
+end
